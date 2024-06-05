84851ab249d324466e7813d34d97acfde207e4bb
Minbro-Kim <ab000701@dgu.edu>
2024-06-05 22:43:18 +0900

    프론트 주소 수정

--- src/frontend/src/Home.js
+++ src/frontend/src/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./css/Home.css"; // CSS 파일 import
 import logo from "./asset/123.png";
+import "./css/Home.css"; // CSS 파일 import
 
 const Home = () => {
   return (
     <div className="Homediv">
       <img className="logo" src={logo} alt="로고" />
       <p className="logotext">3초만에 끝나는 간편한 출석체크</p>
-      <Link to="/login">
+      <Link to="/login" target="_blank">
         <button>시작하기</button>
       </Link>
     </div>
